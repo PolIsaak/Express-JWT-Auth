2dc5b41de720b0d08a449d025e74c3fe7fb575df
Paul Isaac <paulisaak.dev@outlook.com>
2025-01-01 20:40:01 -0700

    fix: enable routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,11 @@
 const express = require('express');
-//const authRoutes = require('./routes/authRoutes');
-//const protected = require('./routes/protectedRoutes');
+const authRoutes = require('./routes/authRoutes');
+const protected = require('./routes/protectedRoutes');
 
 const app = express();
 
 app.use(express.json());
-//app.use('/auth', authRoutes);
-//app.use('/u', protected);
+app.use('/auth', authRoutes);
+app.use('/u', protected);
 
 module.exports = app;
