94553e78a88534e17470406ce5620a913962b445
Paul Isaac <paulisaak.dev@outlook.com>
2025-01-01 21:37:39 -0700

    fix: resolve visual bug with marquee when images ends

--- frontend/src/components/Marquee.tsx
+++ frontend/src/components/Marquee.tsx
@@ -24,12 +24,24 @@ const Marquee = () => {
 
   return (
     <div className='container mx-auto mt-24 lg:mt-0'>
-      <div className='marquee-side-gradient'>
+      <div className='flex marquee-side-gradient'>
         <motion.div 
           initial={{x:0}}
           animate={{x:"-100%"}}
           transition={{duration: 30, repeat: Infinity, ease: "linear"}}
-          className='flex'
+          className='flex flex-shrink-0'
+        >
+          {
+            upperMarquee.map((image, index) => {
+              return <Image className="h-40 w-56 pr-20" src={image} alt='Tech brand logo' key={index} width={64} height={64} />
+            })
+          }
+        </motion.div>
+        <motion.div 
+          initial={{x:0}}
+          animate={{x:"-100%"}}
+          transition={{duration: 30, repeat: Infinity, ease: "linear"}}
+          className='flex flex-shrink-0'
         >
           {
             upperMarquee.map((image, index) => {
